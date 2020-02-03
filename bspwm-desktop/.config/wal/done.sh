c11e16722d36371773225cba7aecab80f72ee97f
Chris Hoage <iam@chrishoage.com>
2020-02-02 19:00:30 -0800

    switch from urxvt to termite

--- bspwm-desktop/.config/wal/done.sh
+++ bspwm-desktop/.config/wal/done.sh
@@ -14,11 +14,14 @@ bspc config focused_border_color  "$color4"
 # Symlink config files so programs can be started
 # without referencing the cache directly
 #
-# Includes: dunst
+# Includes: dunst,termite
 # =================================================
 
 mkdir -p  "${HOME}/.config/dunst"
 ln -sf    "${HOME}/.cache/wal/dunstrc"    "${HOME}/.config/dunst/dunstrc"
+
+mkdir -p  "${HOME}/.config/termite"
+ln -sf    "${HOME}/.cache/wal/termite"    "${HOME}/.config/termite/config"
 
 # ===================
 # ====== dunst ======
