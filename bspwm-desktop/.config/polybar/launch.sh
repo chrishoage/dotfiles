56b99a1fab5783490e8bf5989e46b92c137ca107
Chris Hoage <iam@chrishoage.com>
2020-06-14 18:45:49 -0700

    pick coretemp package hwmon from function

--- bspwm-desktop/.config/polybar/launch.sh
+++ bspwm-desktop/.config/polybar/launch.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# the identifier of the hwmon and thermnal zone of the cpu
+# package changes between reboots, so we must search for
+# the correct sys link and send it to polybar
+get_cpu_package_hwmon() {
+	for i in /sys/class/hwmon/hwmon*/temp*_input; do
+		local label="$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))"
+		if [ "$label" = "coretemp: Package id 0" ]; then
+			echo $(readlink -f $i)
+			break
+		fi
+	done
+}
+
+export CPU_PACKAGE_HWMON="$(get_cpu_package_hwmon)"
+
 # source wal colors.
 source ~/.cache/wal/colors.sh
 
