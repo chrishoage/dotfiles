eeb50fc60912adc02485d5ed3c62b80aaa257676
Chris Hoage <iam@chrishoage.com>
2021-01-08 14:19:59 -0800

    remove unused alias

--- terminal/.zsh/aliases.zsh
+++ terminal/.zsh/aliases.zsh
@@ -1,10 +1,2 @@
-# IP addresses
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias localip="ipconfig getifaddr en1"
-alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
-
-# Enhanced WHOIS lookups
-alias whois="whois -h whois-servers.net"
-
 # git
 alias g="git"
