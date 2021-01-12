3353dd705872e1c329e6d4ed8c4c3a36fc4dd3ba
Chris Hoage <iam@chrishoage.com>
2021-01-11 21:46:33 -0800

    move alacritty wal template and remove termite template

--- bspwm/.config/wal/done.sh
+++ bspwm/.config/wal/done.sh
@@ -10,16 +10,6 @@ source "${HOME}/.cache/wal/colors.sh"
 # Set the border colors
 bspc config focused_border_color  "$color4"
 
-# =================================================
-# Symlink config files so programs can be started
-# without referencing the cache directly
-#
-# Includes: termite
-# =================================================
-
-mkdir -p  "${HOME}/.config/termite"
-ln -sf    "${HOME}/.cache/wal/termite"    "${HOME}/.config/termite/config"
-
 # ===================
 # ====== dunst ======
 # ===================
