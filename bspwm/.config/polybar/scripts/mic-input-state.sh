c3b469887a2eac8b02970e30933ce8eb3db42cb4
Chris Hoage <iam@chrishoage.com>
2021-02-15 14:08:29 -0800

    remove pulseaudio-ctl dep to allow compat with pipewire-pulse

--- bspwm/.config/polybar/scripts/mic-input-state.sh
+++ bspwm/.config/polybar/scripts/mic-input-state.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-MIC_MUTED=$(pulseaudio-ctl full-status | cut -d" " -f3)
+MIC_SOURCE=$(pactl info | sed -En 's/Default Source: (.*)/\1/p')
+MIC_MUTED=$(pactl list sources | grep $MIC_SOURCE -A 12 | grep -c 'Mute: yes')
 
-if [ "$MIC_MUTED" = "no" ]; then
+if [ "$MIC_MUTED" = "0" ]; then
   echo ""
 else
   echo ""
 fi
-
