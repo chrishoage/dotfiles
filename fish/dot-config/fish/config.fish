d89084edf81d0b12060674dddcf8461b48549a25
Chris Hoage <iam@chrishoage.com>
2026-03-13 17:24:19 -0700

    fish conf.d seemed like a good idea at the time

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -1,1 +1,27 @@
+fish_add_path "$HOME/.local/bin"
+
+if test -d /opt/homebrew/bin
+    /opt/homebrew/bin/brew shellenv | source
+end
+
+starship init fish | source
+
 set --global fish_key_bindings fish_default_key_bindings
+
+set nix_profile /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+
+if test -f $nix_profile
+    source $nix_profile
+end
+
+if command -q zoxide
+    zoxide init fish | source
+end
+
+if command -q direnv
+    direnv hook fish | source
+end
+
+if command -q fzf
+    fzf --fish | source
+end
